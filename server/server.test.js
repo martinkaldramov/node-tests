00cb7ebfd74285b722bcdfc0f5b8c5cc8f6f970d
Marto <martinkaldramov1@gmail.com>
2017-09-13 13:11:46 +0300

    added describe blocks to organzie tests code better

--- server/server.test.js
+++ server/server.test.js
@@ -3,28 +3,38 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return Hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({error: 'Page not found.'});      
-    })
-    .end(done);
+describe('Server', () => {
+
+  describe('#GET /', () => {
+    it('should return Hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({error: 'Page not found.'});      
+        })
+        .end(done);
+    });
+  }); 
+
+  describe('#GET /users', () => {
+    it('should check if i exist in the users array', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude(
+            {
+              name: 'Martin',
+              age: 25
+            }
+          );
+        })
+        .end(done);
+    });
+  });
 });
 
-it('should check if i exist in the users array', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude(
-        {
-          name: 'Martin',
-          age: 25
-        }
-      );
-    })
-    .end(done);
-});
+
+
 
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,42 +2,45 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(20, 13);
+describe('Utils', () => {
 
-  expect(res).toBe(33).toBeA('number');
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(5, 7, (res) => {
-    expect(res).toBe(12).toBeA('number');
-    done();
+  it('should add two numbers', () => {
+    var res = utils.add(20, 13);
+  
+    expect(res).toBe(33).toBeA('number');
   });
-});
-
-it('should square a number', () => {
-  var res = utils.square(4);
-
-  expect(res).toBe(16).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(8, (res) => {
-    expect(res).toBe(64).toBeA('number');
-    done();
+  
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(5, 7, (res) => {
+      expect(res).toBe(12).toBeA('number');
+      done();
+    });
   });
-});
-
-it('should verify first and last names are set', () => {
-  var user = utils.setName({
-    location: 'Sofia, Bulgaria',
-    job: 'Tech Support'
-  }, 'Martin Kaldramov');
-
-  expect(user)
-    .toBeA('object')
-    .toInclude({
-      firstName: 'Martin',
-      lastName: 'Kaldramov'
+  
+  it('should square a number', () => {
+    var res = utils.square(4);
+  
+    expect(res).toBe(16).toBeA('number');
   });
+  
+  it('should async square a number', (done) => {
+    utils.asyncSquare(8, (res) => {
+      expect(res).toBe(64).toBeA('number');
+      done();
+    });
+  });
+  
+  it('should verify first and last names are set', () => {
+    var user = utils.setName({
+      location: 'Sofia, Bulgaria',
+      job: 'Tech Support'
+    }, 'Martin Kaldramov');
+  
+    expect(user)
+      .toBeA('object')
+      .toInclude({
+        firstName: 'Martin',
+        lastName: 'Kaldramov'
+    });
+  }); 
 });
