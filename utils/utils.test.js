45385fca3c45830e20f8e1609279f0719a52b887
Marto <martinkaldramov1@gmail.com>
2017-09-13 07:42:16 +0300

    added simple assertions with expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,12 +1,15 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(20, 13);
+
+  expect(res).toBe(33).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(4);
 
-  if(res !== 16)
-    throw new Error(`Expected 16, instead got ${res}`);
+  expect(res).toBe(16).toBeA('number');
 });
