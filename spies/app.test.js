5ca17cd9e8462c2a28843582beb0ab187a94959e
Marto <martinkaldramov1@gmail.com>
2017-09-14 11:50:43 +0300

    aded a spy function to test if a real db save function gets called

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,14 +1,27 @@
 const expect = require('expect');
 const rewire = require('rewire');
 
-var app = rewire('./app');
+var app = rewire('./app.js');
 
 describe('App', () => {
+
+  var db = {
+    saveUser: expect.createSpy()
+  }
+  app.__set__('db', db);
 
   it('should call the spy correctly', () => {
     var spy = expect.createSpy();
     spy();
     expect(spy).toHaveBeenCalled();
+  });
+
+  it('Should call saveuser with an object', () => {
+    var email = 'martinkaldramov@example.com';
+    var password = 'abcdEFG';
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
   });
 
 });
